828e2f95056f9d9e312dd8c86b26bbb5b32033dd
chankruze <chankruze@gmail.com>
2019-09-26 04:34:10 +0530

    Update Concentric Square Printing Algorithm
    * Add a small check which allows only one digits number excluding 0 to print. If double digits are printed the output is no more concentric.
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- solutions/ConcentricSquare.c
+++ solutions/ConcentricSquare.c
@@ -5,22 +5,29 @@
 
 int main(){
     int n, i, j, k, max;
+    printf("Enter a number from 0-9:\n");
     scanf("%d", &n);
+    clrsr();
   	// Complete the code to print the pattern.
     max = 2*(n-1);
 
-    for(i = 0; i <= max; i++){
-        k = n;
-        for(j = 0; j <= max; j++){
-            printf("%d ", k);
-            if(i > j){
-                k -= 1;
+    if (n > 0 && n < 10){
+        for(i = 0; i <= max; i++){
+            k = n;
+            for(j = 0; j <= max; j++){
+                printf("%d ", k);
+                if(i > j){
+                    k -= 1;
+                }
+                if(i + j >= max){
+                    k += 1;
+                }
             }
-            if(i + j >= max){
-                k += 1;
-            }
-        }
-        printf("\n");
+            printf("\n");
+        }        
+    } else{
+        printf("The code still works but the output is no more a concentric square.\nOnly single digits look beautiful.\n");
     }
+    
     return 0;
 }
