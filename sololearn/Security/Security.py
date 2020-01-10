686aa3763518e7106699ed29214986000e981094
chankruze <chankruze@gmail.com>
2020-01-10 11:32:14 +0530

    Add Challenge:  Security [Hard]
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ sololearn/Security/Security.py
@@ -0,0 +1,33 @@
+in_str = str(input())
+
+money = in_str.index('$')
+thief = in_str.index('T')
+guard = 0
+guards = list()
+
+while guard < len(in_str):
+  try:
+    guard = in_str.index('G', guard)
+    guards.append(guard)
+    guard += 1
+  except:
+    guard = len(in_str)
+
+if len(guards) != 0:
+    quiet_count = 0
+    
+    for guard in guards:
+        if money > thief:
+            if guard in range(thief, money):
+                quiet_count += 1
+        else:
+            if guard in range(money, thief):
+                quiet_count += 1
+            
+    if quiet_count != 0:
+        print("quiet")
+    else:
+        print("ALARM")
+
+else:
+    print("ALARM")
